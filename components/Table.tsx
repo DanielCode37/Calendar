0444739d3e7e78efd45b1be32d7754b12dd45140
DanielCode37 <buechner.daniel@gmx.net>
2021-07-23 23:33:33 +0200

    added first coloring

--- components/Table.tsx
+++ components/Table.tsx
@@ -7,17 +7,23 @@ const keyGen = new KeyGenerator;
 export default function Table(props: { data: any[][] }) {
     return (
         <View>
-            {props.data.map(row => { return Row(row) })}
+            {props.data.map(row => { return Row(row, props.data.indexOf(row)) })}
         </View>
     );
 }
 
-function Row(data: any[]): JSX.Element {
+function Row(data: any[], index: Number): JSX.Element {
     return (
         <View style={styles.row}>
-            {data.map(item => { return <Text style={styles.cell} key={keyGen.generateKey()} >{item}</Text> })}
+            {data.map(item => { return <Text style={[styles.cell, { color: calcColor(index, item) }]} key={keyGen.generateKey()} >{item}</Text> })}
         </View>
     );
+}
+
+function calcColor(row: Number, value: Number): "whitesmoke" | "grey" {
+    if (row == 0 && value > 10) return "grey";
+    if ((row == 5 || row == 4) && value < 10) return "grey";
+    return "whitesmoke";
 }
 
 const styles = StyleSheet.create({
@@ -29,6 +35,5 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         width: 50,
         fontSize: 20,
-        color: 'whitesmoke'
     }
 });
